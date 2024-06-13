03a68d4d0fe5ad907249fc2de98e59bace6484fa
Rhythm1821 <alamrawat1821@gmail.com>
2024-06-13 12:03:15 +0530

    project setup

---
+++ backend/src/index.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+// Load environment variables from .env file
+dotenv.config();
+
+const connectionString = process.env.MONGODB_CONNECTION_STRING;
+
+// Check if connection string is loaded
+if (!connectionString) {
+  throw new Error('MONGODB_CONNECTION_STRING is not defined in the environment variables.');
+}
+
+mongoose.connect(connectionString as string)
+  .then(() => console.log('Database connected!'))
+  .catch((err) => console.log('Database connection error:', err));
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/api/test', async (req: Request, res: Response) => {
+  res.json({ message: 'Hello from express.js!' });
+});
+
+app.listen(3000, () => console.log('Server is running on port 3000'));
