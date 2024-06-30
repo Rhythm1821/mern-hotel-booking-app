22e1ca2c480377ebcff550060704768bf42790dd
Rhythm1821 <alamrawat1821@gmail.com>
2024-06-30 15:17:18 +0530

    complete authentication on the frontend

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,8 +1,8 @@
 import { useForm } from "react-hook-form";
-import { useMutation } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import * as apiClient from '../api-client';
 import { useAppContext } from "../contexts/AppContext";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export type RegisterFormData = {
     firstName: string,
@@ -13,16 +13,18 @@ export type RegisterFormData = {
 };
 
 export default function Register() {
+    const queryClient = useQueryClient()
     const navigate = useNavigate()
     const { showToast } = useAppContext()
     const { register, watch, handleSubmit, formState: { errors } } = useForm<RegisterFormData>();
 
     const mutation = useMutation(apiClient.register, {
-        onSuccess: () => {
+        onSuccess: async () => {
             showToast({
                 message: "User created successfully",
                 type: "SUCCESS"
             }),
+            await queryClient.invalidateQueries("validateToken")
             navigate("/")
         },
         onError: (error: Error) => {
@@ -97,7 +99,10 @@ export default function Register() {
                     <span className="text-red-500">{errors.confirmPassword.message}</span>
                 )}
             </label>
-            <span>
+            <span className="flex justify-between items-center">
+                <span className="text-sm">
+                        Already have an account? <Link className="text-blue-600" to="/sign-in">Login here</Link>
+                </span>
                 <button type="submit" className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl">
                     Create Account
                 </button>
